171c7fded1d65426ef31c6b79e8433bcd8fcd7d7
Aimo <2422489737@qq.com>
2024-05-03 01:52:42 +0800

    添加备注,检查打包

--- Source/UniversalFunctionLibrary/Public/UniversalFunctionLibrarys.h
+++ Source/UniversalFunctionLibrary/Public/UniversalFunctionLibrarys.h
@@ -1,9 +1,27 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/**
+* Copyright: Aimo_皑墨
+* Open source protocol: MIT License
+;* Open Source Date: Jun 5, 2023
+* BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
+* making address: https://github.com/AimoTvT/UniversalFunctionLibrary
+* We welcome the contributions of powerful movers and movers to join this plugin
+* Build powerful plugins together!!
+*
+* 版权所有权: Aimo_皑墨
+* 开源协议: MIT License
+* 开源时间: 2023年6月29日
+* BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867
+* GitHub地址: https://github.com/AimoTvT/UniversalFunctionLibrary
+* 欢迎有实力的大佬/萌新加入本插件的贡献
+* 一起打造强大的插件!!!
+*/
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "Engine/Engine.h"
+#include "Engine/EngineTypes.h"
 
 #include "UniversalFunctionLibrarys.generated.h"
 
@@ -142,8 +160,7 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Aimo|Static|Miscellaneous")
 	static FVector2D Vector2DSwapXY(const FVector2D& SwapVector2D);
 
-	//UFUNCTION(BlueprintPure, Category = "Aimo|Static|Miscellaneous")
-	/** * 获取该Actor对应的所有组件 */
+	/** * 获取所有组件 */
 	static TArray<UActorComponent*> GetActorComponents(const AActor* Actor, const TSubclassOf<UActorComponent>& ComponentClass);
 
 
@@ -175,14 +192,18 @@ public:
 	/** * 获取字符串组有效值 */
 	static FString StringsGet(const TArray<FString>& Strings, int Index = 0);
 
+	/** * 获取名字组有效值 */
 	static FName NamesGet(const TArray<FName>& Names, int Index);
 
+	/** * 获取组内部分/全部值 */
 	UFUNCTION(BlueprintPure, Category = "Aimo|Static|Miscellaneous")
 	static TArray<FString> GetStringsScopes(const TArray<FString>& Strings, int Index = 0, int EndIndex = -1);
 
+	/** * 字符串组转名字组 */
 	UFUNCTION(BlueprintPure, Category = "Aimo|Static|Miscellaneous")
 	static TArray<FName> StringsToNames(const TArray<FString>& Strings);
 
+	/** * 名字组转字符串组 */
 	UFUNCTION(BlueprintPure, Category = "Aimo|Static|Miscellaneous")
 	static TArray<FString> NamesToStrings(const TArray<FName>& Strings);
 
@@ -198,6 +219,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Aimo|Static|Miscellaneous")
 	static FVector FrontScopeRay(UObject* World, const FVector& Location, const FVector& Forward, const TEnumAsByte<ETraceTypeQuery>& TraceTypeQuerys, const TArray<AActor*>& ActorsToIgnore, float Distance = 100, float DownDistance = 200, float Scope = 80, int Num = 22);
 
+	/** * 获取XY限制大小 */
 	UFUNCTION(BlueprintPure, Category = "Aimo|Static|Miscellaneous")
 	static FVector2D GetXYClampSize(float X, float Y, float XMax, float YMax);
 
@@ -221,12 +243,12 @@ public:
 
 
 	/** * 生成贴花并映射位置 */
-	static UDecalComponent* SpawnDecalAtLocation(AActor* Owner, UMaterialInstance* MaterialInstance, FVector Location, FRotator Rotation = FRotator(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), float LifeSpan = 0, float FadeScreeSize = 0);
-	static UDecalComponent* SpawnDecalAtLocation(AActor* Owner, UMaterialInstance* MaterialInstance, FVector Location, FVector Rotation = FVector(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), float LifeSpan = 0, float FadeScreeSize = 0);
+	static UDecalComponent* SpawnDecalAtLocation(AActor* Owner, UMaterialInterface* MaterialInterface, FVector Location, FRotator Rotation = FRotator(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), float LifeSpan = 0, float FadeScreeSize = 0);
+	static UDecalComponent* SpawnDecalAtLocation(AActor* Owner, UMaterialInterface* MaterialInterface, FVector Location, FVector Rotation = FVector(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), float LifeSpan = 0, float FadeScreeSize = 0);
 
 	/** * 生成贴花并绑定物体 */
-	static UDecalComponent* SpawnDecalAttached(AActor* Owner, UMaterialInstance* MaterialInstance, USceneComponent* AttachComponent, FName AttachPointName, FVector Location, FRotator Rotation = FRotator(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), EAttachLocation::Type LocationType = EAttachLocation::KeepRelativeOffset, float LifeSpan = 0, float FadeScreeSize = 0);
-	static UDecalComponent* SpawnDecalAttached(AActor* Owner, UMaterialInstance* MaterialInstance, USceneComponent* AttachComponent, FName AttachPointName, FVector Location, FVector Rotation = FVector(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), EAttachLocation::Type LocationType = EAttachLocation::KeepRelativeOffset, float LifeSpan = 0, float FadeScreeSize = 0);
+	static UDecalComponent* SpawnDecalAttached(AActor* Owner, UMaterialInterface* MaterialInterface, USceneComponent* AttachComponent, FName AttachPointName, FVector Location, FRotator Rotation = FRotator(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), EAttachLocation::Type LocationType = EAttachLocation::KeepRelativeOffset, float LifeSpan = 0, float FadeScreeSize = 0);
+	static UDecalComponent* SpawnDecalAttached(AActor* Owner, UMaterialInterface* MaterialInterface, USceneComponent* AttachComponent, FName AttachPointName, FVector Location, FVector Rotation = FVector(0.0f, 0.0f, 0.0f), FVector Scale3D = FVector(1.0f, 1.0f, 1.0f), EAttachLocation::Type LocationType = EAttachLocation::KeepRelativeOffset, float LifeSpan = 0, float FadeScreeSize = 0);
 
 
 
